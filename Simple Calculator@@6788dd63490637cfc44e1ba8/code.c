a7c7b63b43e11ab2ac5f8ec1f2862861c256d899
Rohit Sharma <rohitrohan2424@gmail.com>
2025-01-27 00:25:58 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,7 +4,7 @@ int main() {
     float num1, num2; 
     int div, prod, add, minus;
     char ch;
-    scanf("%d %d %c",&num1, &num2, &ch);
+    scanf("%f %f %c",&num1, &num2, &ch);
     add = num1+num2;
     minus = num1-num2;
     prod = num1*num2;
