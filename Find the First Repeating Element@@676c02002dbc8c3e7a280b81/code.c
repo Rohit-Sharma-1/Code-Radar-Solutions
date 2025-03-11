c3380fc791acbf950c5aa2c57d2187f69349912a
ROHIT SHARMA <rohitrohan2424@gmail.com>
2025-03-11 18:19:01 +0530

    Solution to Find the First Repeating Element

---
+++ Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+    }
+    int temp[n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(arr[j]==temp[i]){
+                printf("%d",arr[j]);
+                return 0;
+            }else{
+                temp[i] = arr[j];
+            }
+        }
+    }
+}
